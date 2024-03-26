032b37793c36c59e36ca4ad4f07bd0d0eef90e35
fabio-coutinho-dos-santos <fabio.santcou@gmail.com>
2024-03-26 09:01:46 -0300

    feat: remove unused code

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductsModule } from './domain/products/products.module';
 import { UsersModule } from './domain/users/users.module';
 
 @Module({
-  imports: [ProductsModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ProductsModule, UsersModule]
 })
 export class AppModule {}
