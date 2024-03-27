d166448fd990a7d0558bfd372b1eb5b28bfe8927
fabio-coutinho-dos-santos <fabio.santcou@gmail.com>
2024-03-27 18:01:11 -0300

    feat: remove unused code

--- src/infrastructure/api/products/controller/products.controller.ts
+++ src/infrastructure/api/products/controller/products.controller.ts
@@ -1,12 +1,9 @@
-import { Body, Controller, Get, Inject, Param, Post } from '@nestjs/common';
+import { Controller, Get, Inject, Param } from '@nestjs/common';
 import { ProductsRepositoryInterface } from '../../../../domain/products/products.repository.interface';
-import { GetCheckoutProduct } from '../../../../use-cases/products/get-checkout-product/get-checkout-product';
 import { UsersRepositoryInterface } from 'src/domain/users/users.repository.interface';
-import { GetProductSeller } from 'src/use-cases/products/get-product-seller/get-product-seller';
 import { Facade } from '../facades/products.facade';
 import { ApiTags } from '@nestjs/swagger';
 import { API_CONFIG } from 'src/config';
-import { ProductType } from 'src/domain/products/types/checkout-products-type';
 
 @Controller(`${API_CONFIG.API_PREFIX}/${API_CONFIG.API_VERSION}/products`)
 @ApiTags('Products')
@@ -19,20 +16,12 @@ export class ProductsController {
   ) {}
 
   @Get(':id')
-  async findById(@Param('id') id: number) : Promise<ProductType> {
-    const product = await new GetCheckoutProduct(this.productRepository).execute(id);
-    const seller = await new GetProductSeller(this.usersRepository).execute(id);
-
-    return {
-      ...product,
-      seller,
-    };
-  }
-
-  @Get('/facade/:productId')
-  async findOne(@Param('productId') productId: number) {
-    const productsFacade = new Facade.Products();
-    const finalProduct = await productsFacade.getProduct(productId);
+  async findById(@Param('id') id: number) {
+    const productsFacade = new Facade.Products(
+      this.productRepository,
+      this.usersRepository,
+    );
+    const finalProduct = await productsFacade.getProduct(id);
     return finalProduct;
   }
 }
--- src/infrastructure/database/typeorm/repositories/users.repository.ts
+++ src/infrastructure/database/typeorm/repositories/users.repository.ts
@@ -1,31 +1,19 @@
 import { UsersRepositoryInterface } from 'src/domain/users/users.repository.interface';
 import { UserModel } from '../mysql/entities/users.model';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm'
+import { Repository } from 'typeorm';
 
 export class UsersRepository implements UsersRepositoryInterface {
-
-  @InjectRepository(UserModel)
-  private readonly usersRepository: Repository<UserModel>
+  constructor(
+    @InjectRepository(UserModel)
+    private readonly usersRepository: Repository<UserModel>,
+  ) {}
 
   create() {
     throw new Error('Method not implemented.');
   }
 
-  // async findOne(id: number) {
-  //   return await this.usersRepository.findOneBy({id});
-  // }
-  
-  findOne(id: number) {
-    return {
-      id: 278,
-      name: 'Dev Greenn Test',
-      cellphone: '+5544998741312',
-      is_heaven: 1,
-      is_greenn: 1,
-      company_id: 1747,
-      internacional_company_id: 4517
-    };
+  async findOne(id: number) {
+    return await this.usersRepository.findOneBy({ id });
   }
-  
 }
